f00ce8f6070682e9a516d3a70cdeb2a922bcc1db
Chandra Manikandan <professionalchandru@gmail.com>
2021-12-07 11:13:37 +0530

    content changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,7 @@ const BlogIndex = ({ data, location }) => {
         <Seo title="All posts" />
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
+          Create a beautiful blog that fits your style. Choose from a selection of easy-to-use templates – all with flexible layouts and hundreds of background images – or design something new.
         </p>
       </Layout>
     )
